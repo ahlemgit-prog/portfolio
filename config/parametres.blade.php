d481000d142b5a03935aad765faf2a9fee5324bf
ahlemgit-prog <71451312+ahlemgit-prog@users.noreply.github.com>
2021-03-16 21:43:31 +0100

    Add files via upload

---
+++ config/parametres.blade.php
@@ -0,0 +1,15 @@
+<?php
+
+/* ./app/config/parametres.blade.php
+    Paramètres de l'application
+*/
+
+//Paramètres de connexion à la base de données
+  define('DBHOTE', '127.0.0.1:3306');
+  define('DBNAME', 'portfolio');
+  define('DBUSER', 'root');
+  define('DBPWD', 'root');
+
+// Zones dynamiques du template général
+
+  $zoneScripts = '';
