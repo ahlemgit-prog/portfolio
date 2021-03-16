96ee3708ef2e01f11e301a5ac74542d36f07727e
ahlemgit-prog <71451312+ahlemgit-prog@users.noreply.github.com>
2021-03-16 21:53:36 +0100

    Add files via upload

---
+++ resources/views/template/articles/xabia.blade.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html class="no-js" lang="fr">
+
+<head>
+   @include('template.partials._head')
+</head>
+
+<body id="body" class="darkmode">
+
+    @include('template.partials._header')
+
+     <div class="bradcam_area bradcam_bg_1">
+         <div class="container">
+             <div class="row">
+                 <div class="col-xl-12">
+                     <div class="bradcam_text text-center">
+                         <h3>Xabia</h3>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </div>
+     <!-- /bradcam_area  -->
+
+   <!--================Blog Area =================-->
+   <section class="blog_area single-post-area section-padding">
+      <div class="container">
+         <div class="row">
+            <div class="col-lg-8 posts-list">
+               <div class="single-post">
+                  <div class="feature-img">
+                    <video class="card-img rounded-0" allowscreen frameborder="0" autoplay controls>
+                      <source src="img/projets/Xabia.mp4" width="610" height="887" type="video/mp4">
+                    </video>
+                  </div>
+                  <div class="blog_details">
+                     <h2>Xabia Residencia</h2>
+                     <ul class="blog-info-link mt-3 mb-4">
+                        <li><a href="#"><i class="fa fa-user"></i> Illustration, Design</a></li>
+                        <li><a href="#"><i class="fa fa-comments"></i> 0 Commentaires</a></li>
+                     </ul>
+                     <p class="excert">
+                        Xabia Residencia est une plateforme de location de logement. En tant que designer, mon job consiste à  comprendre le cheminement de l'utilisateur,
+                        combler ses besoins et réfléchir sur la façon de procéder afin qu'il trouve son bonheur. Rassembler et divertir le projet d'un futur site web n'est pas une tâche facile
+                        mais se mettre dans la peau de l'utilisateur et imaginer son cheminement, tester et trouver des solutions font parties des stratégies d'un bon web designer. <br>
+                        <br>
+                        Pour commencer, je choisis 3 types de palettes de manière simpliste pour accrocher mon thème: les vacances d'été.
+                        Je m'oriente vers les couleurs chaudes: le jaune pour le soleil, la luminosité, la joie, le bleu pour le ciel, la mer et la confiance, le rouge pour la chaleur, l'amour,
+                        le vert pour la nature apaissante. <br>
+                        <br>
+                        Je choisis également la typographie associant avec la marque, le logo ou encore le thème. <br>
+                        La typographie a une signification bien particulière dans le contexte de location de logement. <br>
+                        D'une part, elle doit se démarquer par un certain style de modernité. D'autre part, elle doit prouver son confort, sa confiance pour sa clientèle. <br>
+                        <br>
+                        <div class="feature-img">
+                         <img   class="img-fluid" src="img/projets/typographie.png" alt="" width="730" height="350" text-center> <br>
+                        </div>
+                     </p>
+
+                     <div class="quote-wrapper">
+                        <div class="quotes">
+                          <p>
+                            <i>Adobe XD, comment ajuster les éléments dans le quadrillage en format mobile ? </i><br>
+                            <br>
+                            Pour ajuster les éléments, le quadrillage est un outil essentiel pour l'emplacement des éléments. <br>
+                            Les boutons et la barre de navigation accessibles et fonctionnels doivent apparaitre dans ce fameux quadrillage.
+                            Chaque grille possède un certain nombre d'espaces que l'on appelle gouttière. <br>
+                            Elle permet d'établir une certaine dimension pour un design web, il faut généralement définir des grilles à 12 ou 16 colonnes. <br>
+                            N’hésitez pas à modifier le nombre de colonnes selon les besoins.
+                          <p>
+                        </div>
+                     </div>
+                       <p>
+                         Je procède avec une page de chargement dans laquelle ceci accompagnera l'utilisateur vers l'accueil de l'application.
+                         Ensuite, je m'assure que le header, le main et le footer sont bien présents. <br>
+                         Dans le header, je dois obligatoirement retrouver la barre de navigation pour les grands formats
+                         ou la barre de navigation avec le menu hamburger pour les formats mobiles. <br>
+                         Enfin, j'adopte la dimension des différents élements comme le logo, le texte ou encore les images afin de rendre l'application plus attrayante.
+                     </p>
+                  </div>
+               </div>
+               <div class="navigation-top">
+                  <div class="d-sm-flex justify-content-between text-center">
+                     <!-- <p class="like-info"><span class="align-middle"><i class="fa fa-heart"></i></span> Lily and 4
+                        people like this</p> -->
+                     <div class="col-sm-4 text-center my-2 my-sm-0">
+                        <!-- <p class="comment-count"><span class="align-middle"><i class="fa fa-comment"></i></span> 06 Comments</p> -->
+                     </div>
+                     <!-- <ul class="social-icons">
+                        <li><a href="#"><i class="fa fa-facebook-f"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                        <li><a href="#"><i class="fa fa-behance"></i></a></li>
+                     </ul> -->
+                  </div>
+                  <div class="navigation-area">
+                     <div class="row">
+                        <div
+                           class="col-lg-6 col-md-6 col-12 nav-left flex-row d-flex justify-content-start align-items-center">
+                           <div class="thumb">
+                              <a href="tigre">
+                                 <img class="img-fluid" src="img/post/button_tigre.png" alt="">
+                              </a>
+                           </div>
+                           <div class="arrow">
+                              <a href="tigre">
+                                 <span class="lnr text-white ti-arrow-left"></span>
+                              </a>
+                           </div>
+                           <div class="detials">
+                              <p>Précédent</p>
+                              <a href="tigre">
+                                 <h4>Tigre</h4>
+                              </a>
+                           </div>
+                        </div>
+                        <div
+                           class="col-lg-6 col-md-6 col-12 nav-right flex-row d-flex justify-content-end align-items-center">
+                           <div class="detials">
+                              <p>Suivant</p>
+                              <a href="piccolo">
+                                 <h4>Piccolo</h4>
+                              </a>
+                           </div>
+                           <div class="arrow">
+                              <a href="tigre">
+                                 <span class="lnr text-white ti-arrow-right"></span>
+                              </a>
+                           </div>
+                           <div class="thumb">
+                              <a href="piccolo">
+                                 <img class="img-fluid" src="img/post/button_piccolo.png" alt="">
+                              </a>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               @include('authors.projets')
+
+               @include('commentaires._index')
+            </div>
+            <div class="col-lg-4">
+               <div class="blog_right_sidebar">
+
+                  @include('template.partials._search')
+
+                  @include('categories._index')
+
+                  @include('posts._index')
+
+                  @include('tags._index')
+
+                  @include('template.partials._instagram')
+
+                  @include('newsletters._index')
+               </div>
+            </div>
+         </div>
+      </div>
+   </section>
+   <!--================ Blog Area end =================-->
+
+   @include('template.partials._footer')
+
+   @include('template.partials._scripts')
+
+</body>
+
+</html>
